69baa74a853e8baee58863538c86bf1b90c9415c
Andrii <andriichampion2008@gmail.com>
2024-02-23 19:19:52 +0300

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   main.py

--- main.py
+++ main.py
@@ -1,33 +1,49 @@
+from typing import List
 import requests
 import bs4
 import flet as ft
 
 
+class userProfileContent(ft.UserControl):
+    def __init__(self):
+        super.__init__(self)
+
+
+class App(ft.UserControl):
+    def __init__(self):
+        super().__init__()
+    
+    
+    def build(self):
+        userIdField = ft.TextField(value="", text_align="left", expand=True, label="Steam 32id")
+        searchButton = ft.IconButton(ft.icons.SEARCH_OUTLINED, on_click=self.search)
+        self.dataStats = ft.Column()
+
+        return ft.Row(
+            [
+                userIdField,
+                searchButton
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+            )
+    
+    def search(self, e):
+        print(e)
 
 
 
 
 def main(page: ft.Page):
     page.title = "Dota stats"
+    page.window_min_width=350
+    page.window_min_height=450
     page.window_width = 450
     page.window_height = 650
-    
-    
-    def search(e):
-        print(e)
-        page.update()
-    
-    
-    userIdField = ft.TextField(value="", text_align="left", width=250, label="Steam 32id")
-    searchButton = ft.IconButton(ft.icons.SEARCH_OUTLINED, on_click=search)
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.update()
 
-    page.add(
-        ft.Row(
-            [
-                userIdField,
-                searchButton
-            ],
-            alignment=ft.MainAxisAlignment.CENTER,
-            ))
+    app = App()
+
+    page.add(app)
 
 ft.app(target=main)
